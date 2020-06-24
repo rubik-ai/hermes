82439380e7ddf9bf3c8fe763ae7cb7e972372ff9
itsaashish <aashish@rubik.ai>
2020-06-24 14:08:17 +0530

    integrated swagger ui

--- hermes-management/src/main/java/pl/allegro/tech/hermes/management/HermesManagement.java
+++ hermes-management/src/main/java/pl/allegro/tech/hermes/management/HermesManagement.java
@@ -2,6 +2,7 @@ package pl.allegro.tech.hermes.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class HermesManagement {
--- hermes-management/src/main/java/pl/allegro/tech/hermes/management/config/JerseyResourceConfig.java
+++ hermes-management/src/main/java/pl/allegro/tech/hermes/management/config/JerseyResourceConfig.java
@@ -1,5 +1,8 @@
 package pl.allegro.tech.hermes.management.config;
 
+import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.jaxrs.listing.ApiListingResource;
+import io.swagger.jaxrs.listing.SwaggerSerializers;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
 import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
@@ -7,6 +10,7 @@ import org.glassfish.jersey.servlet.ServletProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.PostConstruct;
 import javax.ws.rs.ApplicationPath;
 
 @ApplicationPath("/")
@@ -24,5 +28,26 @@ public class JerseyResourceConfig extends ResourceConfig {
         property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
         property(ServletProperties.FILTER_STATIC_CONTENT_REGEX, jerseyProperties.getFilterStaticContentRegexp());
         register(RolesAllowedDynamicFeature.class);
+    }
+    @PostConstruct
+    public void init() {
+        // Register components where DI is needed
+        this.configureSwagger();
+    }
+
+    private void configureSwagger() {
+        // Available at localhost:port/api/swagger.json
+        this.register(ApiListingResource.class);
+        this.register(SwaggerSerializers.class);
+        BeanConfig config = new BeanConfig();
+        config.setConfigId("HERMES-SWAGGER");
+        config.setTitle("*Hermes Apis*");
+        config.setVersion("v1");
+        config.setContact("hermes-pubsub");
+        config.setSchemes(new String[] { "http", "https" });
+        config.setBasePath("/");
+        config.setResourcePackage("pl.allegro.tech.hermes.management.api");
+        config.setPrettyPrint(true);
+        config.setScan(true);
     }
 }
