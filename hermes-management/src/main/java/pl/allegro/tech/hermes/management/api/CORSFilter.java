d0cb5d262262bb1e08786a6912bb1fee11750491
Nabeel <nabeel@rubik.ai>
2020-07-23 18:44:17 +0530

    added a couples of origins in cors.

--- hermes-management/src/main/java/pl/allegro/tech/hermes/management/api/CORSFilter.java
+++ hermes-management/src/main/java/pl/allegro/tech/hermes/management/api/CORSFilter.java
@@ -8,6 +8,10 @@ import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Provider
 public class CORSFilter implements ContainerResponseFilter {
@@ -22,8 +26,13 @@ public class CORSFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
         MultivaluedMap<String, Object> headers = responseContext.getHeaders();
-        headers.add("Access-Control-Allow-Origin", corsProperties.getAllowedOrigin());
-        headers.add("Access-Control-Allow-Methods", "POST,PUT,GET,HEAD,DELETE");
+//        headers.add("Access-Control-Allow-Origin", corsProperties.getAllowedOrigin());
+        List<String> originList = Arrays.asList("http://metis.rubik.ai", "http://dev.rubik.ai", "http://localhost:3000", "*.dataos.io");
+        String origin = requestContext.getHeaderString("Origin");
+        if (originList.contains(origin)) {
+            headers.add("Access-Control-Allow-Origin", origin);
+        }
+        headers.add("Access-Control-Allow-Methods", "OPTIONS,POST,PUT,GET,HEAD,DELETE");
         headers.add("Access-Control-Max-Age", "1209600");
         headers.addAll(
                 "Access-Control-Allow-Headers",
